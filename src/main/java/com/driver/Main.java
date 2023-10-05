0d1e1880c43a4e933445bf132f82a9ff3cc482da
amanchauhan1999 <amanchauhanhimanshu@gmail.com>
2023-10-05 13:52:46 +0530

    TRYgit add .

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //  obj.name = "Aman";
+        //System.out.println(obj.name);
+
+        obj.setName("Aman");
+        String ans = obj.getName();
+        System.out.println(ans);
+    }
 }
