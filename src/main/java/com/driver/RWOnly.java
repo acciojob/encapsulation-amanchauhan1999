a5935ac3baa797c714d4c7f7c0007b32fd12b15f
Aman Singh <138305220+amanchauhan1999@users.noreply.github.com>
2023-10-05 14:15:02 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,21 +1,8 @@
 package com.driver;
 
 public class RWOnly {
-<<<<<<< HEAD
-    private String name ;
-=======
-    private String name = "";
 
-<<<<<<< HEAD
-//    public RWOnly(String name) {
-//        this.name = name;
-//    }
-=======
-    public RWOnly(String name) {
-        this.name = name;
-    }
->>>>>>> 8dba0ef6fc43aef05786dee159944cc5f960f4bd
->>>>>>> 755352647b2afaa27ce3d5993e28dd045d8568d3
+    private String name ;
 
     public String getName() {
         return name;
