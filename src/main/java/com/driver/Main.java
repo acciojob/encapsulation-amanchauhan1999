8dba0ef6fc43aef05786dee159944cc5f960f4bd
amanchauhan1999 <amanchauhanhimanshu@gmail.com>
2023-10-05 13:03:21 +0530

    try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly("Aman");
+
+        System.out.println(rwOnly.getName());
+
+        rwOnly.setName("Himanshu");
+        System.out.println(rwOnly.getName());
+    }
   
 }
